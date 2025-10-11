adc4c8409486fa834e6aca5aadb0cc6607ace392
u0u0io4rg4o0b-lab <u0u0io4rg4o0b@gmail.com>
2025-10-12 03:51:35 +0800

    feat: rent hashing, log verifier, payout pre-check; fix(members) handler scope

--- JS/nav.js
+++ JS/nav.js
@@ -8,6 +8,7 @@
       '<a href="rent.html">租約雜湊</a>',
       '<a href="anchor.html">T-Proof 錨定</a>',
       '<a href="payout_verify.html">撥款前核驗</a>',
+         '<a href="verify.html">日誌驗鍊</a>',
     '</nav>',
     '<hr/>'
   ].join('');
